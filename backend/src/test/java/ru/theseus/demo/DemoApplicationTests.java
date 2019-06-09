456930c68d67f6a94b9dd5fad8b8672a1be4549f
Sergey <pushkinsergey.voronezh@gmail.com>
2019-06-09 12:12:00 +0300

    new backend with Dockerfile

--- backend/src/test/java/ru/theseus/demo/DemoApplicationTests.java
+++ backend/src/test/java/ru/theseus/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.tesey;
+package ru.theseus.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TeseyApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
